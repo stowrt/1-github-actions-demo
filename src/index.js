656bbf4263f77b47961062fb3e44317e53e6ff81
stowrt <storywritterbooks@gmail.com>
2020-09-13 21:50:30 +0530

    Init commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 http
   .createServer(function (req, res) {
     axios.get("https://stowrt-node-example.fly.dev").then((resp) => {
+      console.log(res.data);
       res.write(resp.data);
       res.end();
     });
